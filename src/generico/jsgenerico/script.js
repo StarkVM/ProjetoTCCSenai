42c074d18fa7874f9c6a981cdb3a407bcfcf0b7a
Sal-DS <matheussalda2108@gmail.com>
2026-03-05 10:37:36 -0300

    a

---
+++ src/generico/jsgenerico/script.js
@@ -0,0 +1,50 @@
+//VIACEP API
+let cepField = document.getElementById("cep");
+
+if(cepField){
+  cepField.addEventListener("input", function(){
+      const cep = cepField.value
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.erro) {
+          document.getElementById("cepErro").innerText = "CEP não encontrado!";
+          document.getElementById("cep").value = "";
+          console.log("CEP não encontrado!");
+      
+        } else {
+          console.log(data); 
+
+          document.getElementById('rua').value = data.logradouro;
+          document.getElementById('bairro').value = data.bairro;
+          document.getElementById('cidade').value = data.localidade;
+          document.getElementById('estado').value = data.uf;
+        }
+      })
+      .catch(error => {
+        console.error("Erro na requisição:", error);
+      });
+  });
+}
+
+function mostrarSenha() {
+            const senha = document.getElementById("senha");
+            if (senha.type === "password") {
+                senha.type = "text";
+                document.getElementById("mostrarsenha").innerText = "Esconder";
+            } else {
+                senha.type = "password";
+                document.getElementById("mostrarsenha").innerText = "Mostrar";
+            }
+        }
+
+/*MENU HAMBÚRGUER
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+*/
