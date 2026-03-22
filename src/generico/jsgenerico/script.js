fc01513c924722a331fa0a47cf21663636a3385c
Sal-DS <matheussalda2108@gmail.com>
2026-03-22 16:05:19 -0300

    Adicionei o campo 'confirme sua senha'

--- src/generico/jsgenerico/script.js
+++ src/generico/jsgenerico/script.js
@@ -1,3 +1,12 @@
+//Limpar campos ao carregar a página, isso evita alguns bugs:
+document.addEventListener("DOMContentLoaded", () => {
+  cpfField.value = null;
+  cepField.value = null;
+  senha1.value = null;
+  senha2.value = null;
+})
+
+
 //VIACEP API
 let cepField = document.getElementById("cep");
 let button = document.getElementById("submitbutton");
@@ -33,16 +42,82 @@ if(cepField){
   });
 }
 
-function mostrarSenha() {
-            const senha = document.getElementById("senha");
-            if (senha.type === "password") {
-                senha.type = "text";
-                document.getElementById("mostrarsenha").innerText = "Esconder";
-            } else {
-                senha.type = "password";
-                document.getElementById("mostrarsenha").innerText = "Mostrar";
-            }
-        }
+//olhinho do "mostrar senha"
+function mostrarSenha(pswrd, eye) {
+  const senha = document.getElementById(pswrd);
+  const elemento = document.getElementById(eye);
+  if (senha.type === "password") {
+      senha.type = "text";
+      elemento.classList.remove("bi-eye-slash")
+      elemento.classList.add("bi-eye");
+      
+  } else {
+      senha.type = "password";
+      elemento.classList.remove("bi-eye")
+      elemento.classList.add("bi-eye-slash");
+  }
+}
+
+//Confirme sua sneha
+let senha1 = document.getElementById("senha1");
+let senha2 = document.getElementById("senha2");
+
+if(senha1 || senha2){
+
+  senha1.addEventListener("input", () => confirmarSenha());
+  senha2.addEventListener("input", () => confirmarSenha());
+  
+}
+
+function confirmarSenha(){
+  console.log("bostas")
+  if(senha1.value == senha2.value){
+    button.disabled = false;
+  }
+  else{
+    console.log("Senha diferente");
+    button.disabled = true;
+  }
+}
+
+//VERIFICAÇÃO DE CPF
+let cpfField = document.getElementById("cpf");
+
+if(cpfField){
+  cpfField.addEventListener("input", () => {
+  let cpf = cpfField.value.replace(/\D/g, "");
+  
+  let valido = TestaCPF(cpf);
+  
+  if(valido){
+    button.disabled = false;
+  }
+  else{
+    button.disabled = true;
+  }
+});
+}
+
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (let i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+  Soma = 0;
+    for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
+}
 
 /*MENU HAMBÚRGUER
 
