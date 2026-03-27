12229f4c8f5deec8d574cb2b718807c6e66a6a42
Misael Cardoso <misaelcardoso778@gmail.com>
2026-03-27 10:41:50 -0300

    Adicionado o salvamento de estado das páginas.

---
+++ src/controllers/ondeParou.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+if (isset($data['url']) && !empty($data['url'])) {
+
+    // salva na sessão
+    $_SESSION['ultima_url'] = $data['url'];
+    $_SESSION['tempo_saida'] = time();
+
+    // salva no cookie
+    setcookie("ultima_url", $data['url'], [
+        'expires' => time() + 900, // 15 minutos
+        'path' => '/',
+        'httponly' => true,
+        'samesite' => 'Lax'
+    ]);
+}
+
+
+
+
+
+
+
+
